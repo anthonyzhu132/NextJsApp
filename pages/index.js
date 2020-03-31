027b885588b05e50dffb53ced01c9824841fff66
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-03-31 00:44:17 -0700

    added comments to show steps to make fetch calls to coin desk API

--- pages/index.js
+++ pages/index.js
@@ -2,29 +2,33 @@
 import Layout from '../components/Layout';
 import Fetch from 'isomorphic-unfetch';
 
+import Prices from '../components/Prices';
+
 const index = (props) => (
   <Layout>
     <div>
-      <h1>Welcome to BTC Daily</h1>
+      <h1>Welcome to BitzPrice</h1>
+      <p>Check current Bitcoin rate</p>
+      <Prices bpi={props.bpi}/>
     </div>
   </Layout>
 );
 
 //Next has built in props method to set initial props right away
 //Creating an async function
-index.getIntiailProps = async function() {
+
+index.getInitialProps = async function() {
+
   //setting result as the fetch result from the API call to coin desk
-  const result = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
-  
+  const res = await fetch('https://api.coindesk.com/v1/bpi/currentprice.json');
+
   //setting data as the results's fetch in JSON format
-  const data = await result.json();
+  const data = await res.json();
 
-
-  //returning the object(JSON) data, that has BPI as a key and the value which is the currency and price.
+//returning the object(JSON) data, that has BPI as a key and the value which is the currency and price.
   return {
     bpi: data.bpi
-  }
+  };
+}
 
-};
-
-export default index; 
+export default index;
