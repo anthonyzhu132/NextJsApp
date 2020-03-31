8e6e1f751327a766a270cbe3134fd13738db741a
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-03-31 00:27:34 -0700

    added fetch code to assign all data to const data

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,30 @@
 //Root page or index page
 import Layout from '../components/Layout';
+import Fetch from 'isomorphic-unfetch';
 
-const index = () => (
+const index = (props) => (
   <Layout>
     <div>
       <h1>Welcome to BTC Daily</h1>
     </div>
   </Layout>
 );
+
+//Next has built in props method to set initial props right away
+//Creating an async function
+index.getIntiailProps = async function() {
+  //setting result as the fetch result from the API call to coin desk
+  const result = await fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+  
+  //setting data as the results's fetch in JSON format
+  const data = await result.json();
+
+
+  //returning the object(JSON) data, that has BPI as a key and the value which is the currency and price.
+  return {
+    bpi: data.bpi
+  }
+
+};
 
 export default index; 
