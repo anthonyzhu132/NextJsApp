8ac6755617df92fd3a5d7b31d786044b47b01120
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-03-30 23:32:08 -0700

    added nav for about page

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,10 @@
 //Rendering about page
+import Link from 'next/link';
 
 const about = () => (
   <div>
+    <li><Link href="/"><a>Home</a></Link></li>
+    <li><Link href="/about"><a>About</a></Link></li>
     <h1>About BTC Daily</h1>
     <p>Application to view Bitcoin prices!</p>
   </div>
