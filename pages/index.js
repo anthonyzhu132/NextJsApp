0701394c1da77c38b594deff51d1970954c440ab
anthonyzhu132 <anthonyzhu132@gmail.com>
2020-03-30 23:30:43 -0700

    added Link from Next and created Nav to navigate to other pages

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 //Root page or index page
+import Link from 'next/link';
 
 const index = () => (
   <div>
+    <li><Link href="/"><a>Home</a></Link></li>
+    <li><Link href="/about"><a>About</a></Link></li>
     <h1>Welcome to BTC Daily</h1>
   </div>
 );
